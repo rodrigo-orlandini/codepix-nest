d5fc84e3e67e828ae6c2cca63a9e85a298870a04
esoft41 <rodrigosorlandini@gmail.com>
2023-10-20 15:15:34 -0300

    FEAT: TypeORM settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [BankAccountsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+			host: 'database',
+			database: 'nest',
+			username: 'postgres',
+			password: 'root',
+			entities: [],
+			synchronize: true
+    }),
+    BankAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
